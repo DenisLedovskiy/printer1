6cd552e2efe97d4a678840ef80b1612061cd06be
AnnieKov <80928101+AnnieKov@users.noreply.github.com>
2025-05-01 22:31:40 +0300

    upd

--- Config.swift
+++ Config.swift
@@ -2,10 +2,10 @@ import Foundation
 
 enum Config: String {
 
-    case appID = "112312321"
-    case bundleID = "bundle"
-    case privacy = "https://.google.com/"
-    case term = "https://docs.google.com"
-    case email = "123@gmail.com"
-    case appHud = ""
+    case appID = "6745220570"
+    case bundleID = "smart.printer.doc"
+    case privacy = "https://docs.google.com/document/d/1kTHr8QWFf25YwGt1ps5vd9aVMUN5nLFyU62JEFBL708/edit?usp=sharing"
+    case term = "https://docs.google.com/document/d/1hoSk7KVsoOZB84wby7BFzYsWBMTD7RZu9OoPZ6fhUMc/edit?usp=sharing"
+    case email = "leliaemmetter576@gmail.com"
+    case appHud = "app_9DeMvgLA953NHijJ7f6sJQ63nNB6Lb"
 }
